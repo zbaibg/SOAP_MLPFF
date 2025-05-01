7fc45d513cddfe0e2ea245e9b00a27a944b94bc2
zbaibg <zbai29@wisc.edu>
2025-05-01 03:17:42 -0500

    get trainable parameteers in read_k_fold_loss.py

--- code/read_k_fold_loss.py
+++ code/read_k_fold_loss.py
@@ -1,8 +1,13 @@
 import torch
 import glob
 import pandas as pd
-df=pd.DataFrame(columns=['fold','epoch','train_loss','train_energy_loss','train_force_loss','val_loss','val_energy_loss','val_force_loss','learning_rate'])
+from model import load_model
+df=pd.DataFrame(columns=['fold','epoch','train_loss','train_energy_loss','train_force_loss','val_loss','val_energy_loss','val_force_loss','learning_rate','trainable_parameter'])
 fold_cross_validation=10
+
+model_file=glob.glob('best_fold*_losses_epoch_*.pt')[0]
+model=load_model(model_file)
+trainable_parameter=sum(p.numel() for p in model.energy_net.parameters() if p.requires_grad)
 for i in range(1,fold_cross_validation+1):
     files=glob.glob(f'best_fold{i}_losses_epoch_*.pt')
     assert len(files)==1
@@ -16,7 +21,7 @@ for i in range(1,fold_cross_validation+1):
     df.loc[i,'val_energy_loss']=torch.load(file)['val_energy_loss']
     df.loc[i,'val_force_loss']=torch.load(file)['val_force_loss']
     df.loc[i,'learning_rate']=torch.load(file)['lr']
-
+    
 ave_epoch=df['epoch'].mean()
 ave_train_loss=df['train_loss'].mean()
 ave_train_energy_loss=df['train_energy_loss'].mean()
@@ -54,4 +59,8 @@ df.loc[fold_cross_validation+2,'val_loss']=std_val_loss
 df.loc[fold_cross_validation+2,'val_energy_loss']=std_val_energy_loss
 df.loc[fold_cross_validation+2,'val_force_loss']=std_val_force_loss
 df.loc[fold_cross_validation+2,'learning_rate']=std_learning_rate
+
+df.loc[fold_cross_validation+3,'fold']='trainable_parameter'
+df.loc[fold_cross_validation+3,'trainable_parameter']=trainable_parameter
+
 df.to_csv('k_fold_loss.csv',index=False)
